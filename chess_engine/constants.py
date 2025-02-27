e8c4860f254c38ecf2865484ad8d8616c4b05a1c
Jankoetf <jankomitrovicetf@gmail.com>
2025-02-27 18:52:13 +0100

    creating main package: chess_engine, changed names of files

---
+++ chess_engine/constants.py
@@ -0,0 +1,11 @@
+Width = 64*8;
+Height = 64*8;
+Add_Height = 64;
+Add_Width = 64;
+Width_all = Width + 2*Add_Width;
+Height_all = Height + 2*Add_Height
+Dim = 8;
+Add_Dim = 2;
+Square_size = 64;
+Max_fps = 15;
+

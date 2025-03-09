f16640ed8726b953b705c6b3cee6c8bd89b9ffb2
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-09 23:34:29 +0100

    first run script changed: checking if I'm already inside Chess_Engine folder

--- RunScripts/first_run_automated.bat
+++ RunScripts/first_run_automated.bat
@@ -1,5 +1,11 @@
 @echo off
 
+REM Check if the current directory name is "Chess_Engine"
+for %%* in (.) do set currentDir=%%~n*
+if /I not "%currentDir%"=="Chess_Engine" (
+    cd Chess_Engine
+)
+
 REM Create virtual environment
 python -m venv venv
 REM Activate virtual environment
